ac33efb6af2f1ec13aea39b95c03a3cd3f764442
LeeW <cl87654321@gmail.com>
2017-03-23 21:53:57 +0800

    Implement WebhookUrlForm

---
+++ echobot/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import URL, Required
+
+
+class WebhookUrlForm(FlaskForm):
+    webhook_url = StringField('Webhook Url', validators=[URL(), Required()])
+    submit = SubmitField('Submit')
